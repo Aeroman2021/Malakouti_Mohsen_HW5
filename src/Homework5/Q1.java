3308073d016bf6c96318d791d12fadd1b9e53b75
Mohsen <malakouti.aero@gmail.com>
2021-09-07 12:28:47 +0430

    This is the first commit. Some extra features including Arrange, reverse Arrange , and delete by item added to the code. Q1 is the code for the first question of HW1.The code is  recursive method capable of determine whether a given number is prime or not.

---
+++ src/Homework5/Q1.java
@@ -0,0 +1,29 @@
+package Homework5;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i + " : ");
+            isPrime(i, 2);
+        }
+
+    }
+
+    public static void isPrime(int number, int initialNumber) {
+        if(number == 0 || number == 1)
+            System.out.println("This is not a prime number");
+        else if(number == 2)
+            System.out.println("This is a prime number.");
+        else if (number > initialNumber) {
+            if (number % initialNumber == 0) {
+                System.out.println("This is a prime number.");
+            } else {
+                initialNumber++;
+                isPrime(number, initialNumber);
+            }
+        } else
+            System.out.println("This is not a prime number!");
+    }
+
+}
