12f668416d27662fee06dec7b1a5bdb80286252d
Aeroman2021 <70517731+Aeroman2021@users.noreply.github.com>
2021-09-07 13:41:13 +0430

    Update Q1.java
    Some minor changes were made in this file.

--- src/Homework5/Q1.java
+++ src/Homework5/Q1.java
@@ -17,13 +17,13 @@ public class Q1 {
             System.out.println("This is a prime number.");
         else if (number > initialNumber) {
             if (number % initialNumber == 0) {
-                System.out.println("This is a prime number.");
+                System.out.println("This is not a prime number.");
             } else {
                 initialNumber++;
                 isPrime(number, initialNumber);
             }
         } else
-            System.out.println("This is not a prime number!");
+            System.out.println("This is a prime number!");
     }
 
 }
